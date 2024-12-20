9aa088fe04735502766a3472f10252d102bbd20e
sirichartchai <gs3802@nyu.edu>
2024-12-19 19:20:16 -0500

    feature - Add function to calculate most frequent values in specified columns

--- stats_generation.py
+++ stats_generation.py
@@ -4,8 +4,33 @@
 # TO-DO: teammate comparisons
 # only process new files --> use file timestamps
 
+import pandas as pd
 
 
+def get_battle_stats(file_path):
+    
+    stats = {}
+    data = pd.read_csv(file_path)
+    
+    
+    
+    get_favorites(stats,data)
+    
+    
+def get_most_freq(stats,data):
+    
+    columns = ['event_mode','player_brawler_name','event_map']
+    
+    for column in columns:
+        stats["most_freq_"+column] = data[column].value_counts().idxmax()
+    
+    print(stats)
+
+    
+    
+    
+        
 
 if __name__ == "__main__":
-    print("Stats")
+    fp = "./created-logs-per-player/QJ092J8CU-battle-log-custom.csv"
+    get_battle_stats(fp)
