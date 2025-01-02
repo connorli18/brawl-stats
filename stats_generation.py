48a7394d5d28380c54ceba5ed355ed2f9228002a
sirichartchai <gs3802@nyu.edu>
2025-01-01 23:43:05 -0500

    feat - add win rate analysis to stat_gen.py

--- stats_generation.py
+++ stats_generation.py
@@ -5,32 +5,86 @@
 # only process new files --> use file timestamps
 
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+    
+# def get_event_stats(df):
+    
+#     event_modes = set(df["event_mode"])
+    
+#     print(df.groupby('event_mode').agg({'battle_rank':'mean','battle_trophy_change':'mean'}))
+    
+    
+#     print(df.groupby('battle_result').count())
 
+def get_win_rates(df):
+    
+    def calc_wr(wins,loss):
+        return wins / (wins+loss) if (wins+loss) else 0 
+    
+    # Total all wins
+    arcade_wins = df.battle_result.value_counts()['victory']
+    soloSS_wins = df[(df['event_mode'] == 'soloShowdown') & (df['battle_rank'] <= 4)]['battle_rank'].count()
+    duoSS_wins = df[(df['event_mode'] == 'duoShowdown') & (df['battle_rank'] <= 2)]['battle_rank'].count()
+    trioSS_wins = df[(df['event_mode'] == 'trioShowdown') & (df['battle_rank'] <= 2)]['battle_rank'].count()
+    ttl_win = arcade_wins + soloSS_wins + duoSS_wins + trioSS_wins
+    
+    # Total All losses 
+    arcade_loss = df.battle_result.value_counts()['defeat']
+    soloSS_loss = df[(df['event_mode'] == 'soloShowdown') & (df['battle_rank'] > 4)]['battle_rank'].count()
+    duoSS_loss = df[(df['event_mode'] == 'duoShowdown') & (df['battle_rank'] > 2)]['battle_rank'].count()
+    trioSS_loss = df[(df['event_mode'] == 'trioShowdown') & (df['battle_rank'] > 2)]['battle_rank'].count()
+    ttl_loss = arcade_loss + soloSS_loss + duoSS_loss + trioSS_loss
+    
+    # Total Battles Played (W + L)
+    overall_win_rate = calc_wr(ttl_win,ttl_loss)
+    arcade_win_rate = calc_wr(arcade_wins,arcade_loss) 
+    soloSS_win_rate = calc_wr(soloSS_wins,soloSS_loss) 
+    duoSS_win_rate = calc_wr(duoSS_wins,duoSS_loss) 
+    trioSS_win_rate = calc_wr(trioSS_wins,trioSS_loss)
+    
+    print(f'''Win Rates:
+    \tOverall:{round(overall_win_rate,2)}
+    \tArcade:{round(arcade_win_rate,2)}
+    \tSoloSS:{round(soloSS_win_rate,2)}
+    \tDuoSS:{round(duoSS_win_rate,2)}
+    \tTrioSS:{trioSS_win_rate if trioSS_win_rate else 0}
+    ''')
 
-def get_battle_stats(file_path):
+def get_battle_stats(df):
+    '''
+    TODO: Avg. Trophy gain/loss
+    TODO: Win Streak
+    TODO: Most used brawler 
+    '''
     
-    stats = {}
-    data = pd.read_csv(file_path)
+    get_win_rates(df)
     
-    
-    
-    get_favorites(stats,data)
-    
-    
-def get_most_freq(stats,data):
-    
-    columns = ['event_mode','player_brawler_name','event_map']
-    
-    for column in columns:
-        stats["most_freq_"+column] = data[column].value_counts().idxmax()
-    
-    print(stats)
 
     
+
+def stat_gen(player_tag):
     
-    
+    try:
+        battle_logs_df = pd.read_csv(f'created-logs-per-player/{player_tag}-battle-log-custom.csv')
+    except FileNotFoundError:
+        print(f"Couldn't find file at: created-logs-per-player/{player_tag}-battle-log-custom.csv")
         
+    try:
+        player_info_df = pd.read_csv(f'created-player-info-log/{player_tag}-player-log-custom.csv')
+    except FileNotFoundError:
+        print(f"Couldn't find file at: created-player-info-log/{player_tag}-player-log-custom.csv")
+        
+    get_battle_stats(battle_logs_df)
+
+
+
+    
+
 
 if __name__ == "__main__":
-    fp = "./created-logs-per-player/QJ092J8CU-battle-log-custom.csv"
-    get_battle_stats(fp)
+
+    stat_gen('2GL8YLVPGY')
+    
+    
