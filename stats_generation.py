7d5af4d7c9ce35e11eb4ea1a67aaa065fdb40da4
sirichartchai <gs3802@nyu.edu>
2025-01-03 17:13:04 -0500

    refactor - applied functional approach to _calc_win_rate

--- stats_generation.py
+++ stats_generation.py
@@ -2,87 +2,73 @@
 # TO-DO: track various player things (e.g. brawlers used, star players, favorite events, etc.)
 # TO-DO: teammate comparisons
 # only process new files --> use file timestamps
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import os
     
-# def get_event_stats(df):
+def get_win_rates(df) -> dict:
     
-#     event_modes = set(df["event_mode"])
-    
-#     print(df.groupby('event_mode').agg({'battle_rank':'mean','battle_trophy_change':'mean'}))
-    
-#     print(df.groupby('battle_result').count())
+    win_rates, gamemodes = {}, set(df.event_mode)
 
-def get_win_rates(df):
+    gamemodes.discard('unknown')
+    gamemodes.discard(np.nan)
     
-    def calc_wr(wins,loss):
-        return wins / (wins+loss) if (wins+loss) else 0 
+    showdown_places = {'soloShowdown':4,'duoShowdown':2,'trioShowdown':2,}
     
-    # Total all wins
-    arcade_wins = df.battle_result.value_counts()['victory']
-    soloSS_wins = df[(df['event_mode'] == 'soloShowdown') & (df['battle_rank'] <= 4)]['battle_rank'].count()
-    duoSS_wins = df[(df['event_mode'] == 'duoShowdown') & (df['battle_rank'] <= 2)]['battle_rank'].count()
-    trioSS_wins = df[(df['event_mode'] == 'trioShowdown') & (df['battle_rank'] <= 2)]['battle_rank'].count()
-    ttl_win = arcade_wins + soloSS_wins + duoSS_wins + trioSS_wins
+    def _calc_win_rate(gamemode:str,place=None) -> float:
+        w = l = 0
+        if place:
+            w = df[(df['event_mode'] == gamemode) & (df['battle_rank'] <= place)]['battle_rank'].count()
+            l = df[(df['event_mode'] == gamemode) & (df['battle_rank'] > place)]['battle_rank'].count()
+        else:
+            try:
+                w = df[(df['event_mode'] == gamemode)]['battle_result'].value_counts()['victory']
+            except KeyError:
+                print("Zero Wins in ",gamemode)
+            try:                
+                l = df[(df['event_mode'] == gamemode)]['battle_result'].value_counts()['defeat']
+            except KeyError:
+                print("Zero Losses in",gamemode)
+        return round((w / (w+l) if (w+l) else 0),2)
     
-    # Total All losses 
-    arcade_loss = df.battle_result.value_counts()['defeat']
-    soloSS_loss = df[(df['event_mode'] == 'soloShowdown') & (df['battle_rank'] > 4)]['battle_rank'].count()
-    duoSS_loss = df[(df['event_mode'] == 'duoShowdown') & (df['battle_rank'] > 2)]['battle_rank'].count()
-    trioSS_loss = df[(df['event_mode'] == 'trioShowdown') & (df['battle_rank'] > 2)]['battle_rank'].count()
-    ttl_loss = arcade_loss + soloSS_loss + duoSS_loss + trioSS_loss
+    for gamemode in gamemodes:
+        win_rates[gamemode+"_win_rate"] = _calc_win_rate(gamemode,showdown_places.get(gamemode,None))
     
-    # Total Battles Played (W + L)
-    overall_win_rate = calc_wr(ttl_win,ttl_loss)
-    arcade_win_rate = calc_wr(arcade_wins,arcade_loss) 
-    soloSS_win_rate = calc_wr(soloSS_wins,soloSS_loss) 
-    duoSS_win_rate = calc_wr(duoSS_wins,duoSS_loss) 
-    trioSS_win_rate = calc_wr(trioSS_wins,trioSS_loss)
-    
-    print(f'''Win Rates:
-    \tOverall:{round(overall_win_rate,2)}
-    \tArcade:{round(arcade_win_rate,2)}
-    \tSoloSS:{round(soloSS_win_rate,2)}
-    \tDuoSS:{round(duoSS_win_rate,2)}
-    \tTrioSS:{trioSS_win_rate if trioSS_win_rate else 0}
-    ''')
+    return win_rates
 
-def get_battle_stats(df):
+        
+
+def get_battle_stats(df,stats):
     '''
     TODO: Win Streak
     '''
     
+    
     get_win_rates(df)
     
+    # Calculate Win Streaks
+    # gay = df[df['event_mode'] == ('soloShowdown')]
+    # print(gay)
+    
+
+    
     # Most Used Brawler
-    print("Main Brawler:",df['player_brawler_name'].value_counts().idxmax())
+    stats["Main_Brawler"] = df['player_brawler_name'].value_counts().idxmax()
+
     
     # Avg Trophy gain/loss
-    print("Avg. trophy change", round(df.battle_trophy_change.mean(),2))
-    df.plot(x='date_formatted',y='battle_trophy_change')
-    plt.show()
-    
-
-    
-    
-
-    
-    
-    
-    
-    
-    
-
+    stats["avg_trophy_change"] = round(df.battle_trophy_change.mean(),2)
+    # df.plot(x='date_formatted',y='battle_trophy_change')
+    # plt.show()
     
 
 def stat_gen(player_tag):
     
+    stats = {}
+    
     try:
         battle_logs_df = pd.read_csv(f'created-logs-per-player/{player_tag}-battle-log-custom.csv')
-        battle_logs_df['date_formatted'] = pd.to_datetime(battle_logs_df['battle_time']).dt.strftime('%Y-%m-%d')
+        # battle_logs_df['date_formatted'] = pd.to_datetime(battle_logs_df['battle_time']).dt.strftime('%Y-%m-%d')
     except FileNotFoundError:
         print(f"Couldn't find file at: created-logs-per-player/{player_tag}-battle-log-custom.csv")
         
@@ -91,15 +77,8 @@ def stat_gen(player_tag):
     except FileNotFoundError:
         print(f"Couldn't find file at: created-player-info-log/{player_tag}-player-log-custom.csv")
         
-    get_battle_stats(battle_logs_df)
-
-
-
-    
+    get_battle_stats(battle_logs_df,stats)
 
 
 if __name__ == "__main__":
-
     stat_gen('2GL8YLVPGY')
-    
-    
