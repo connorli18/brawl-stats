cf2522260a2955c7ae2c9a3c1b12c32f18e2157f
sirichartchai <gs3802@nyu.edu>
2025-01-03 23:53:59 -0500

    feat - add overall win rate calculation to win rate stats

--- stats_generation.py
+++ stats_generation.py
@@ -5,34 +5,49 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-    
-def get_win_rates(df) -> dict:
-    
-    win_rates, gamemodes = {}, set(df.event_mode)
 
-    gamemodes.discard('unknown')
-    gamemodes.discard(np.nan)
+
+def get_streaks(df:pd.DataFrame,gamemodes:set,showdown_places:dict):
     
-    showdown_places = {'soloShowdown':4,'duoShowdown':2,'trioShowdown':2,}
+    streaks = {}
+    
+
+    for mode in gamemodes:
+        print(mode.upper())
+        print(df[(df['event_mode'] == mode)].head(10)['battle_result'])
+    
+    # df.assign(streak=(df['battle_result'] != df['WL'].shift()).cumsum())
+
+    
+def get_win_rates(df:pd.DataFrame,gamemodes:set,showdown_places:dict) -> dict:
+    
+    win_rates = {}
     
     def _calc_win_rate(gamemode:str,place=None) -> float:
+        
         w = l = 0
+        
         if place:
             w = df[(df['event_mode'] == gamemode) & (df['battle_rank'] <= place)]['battle_rank'].count()
             l = df[(df['event_mode'] == gamemode) & (df['battle_rank'] > place)]['battle_rank'].count()
         else:
             try:
                 w = df[(df['event_mode'] == gamemode)]['battle_result'].value_counts()['victory']
-            except KeyError:
-                print("Zero Wins in ",gamemode)
-            try:                
                 l = df[(df['event_mode'] == gamemode)]['battle_result'].value_counts()['defeat']
             except KeyError:
-                print("Zero Losses in",gamemode)
-        return round((w / (w+l) if (w+l) else 0),2)
+                print("Zero Wins or Zero Losses in ",gamemode)
+        return (round((w / (w+l) if (w+l) else 0),2),w,l)
+    
+    w_total = l_total = 0
     
     for gamemode in gamemodes:
-        win_rates[gamemode + "_win_rate"] = _calc_win_rate(gamemode,showdown_places.get(gamemode,None))
+        
+        (rate,curr_w,curr_l) = _calc_win_rate(gamemode,showdown_places.get(gamemode,None))
+        win_rates[gamemode + "_win_rate"] = rate
+        w_total += curr_w
+        l_total += curr_l
+    
+    win_rates['overall'] = ((w_total / (w_total+l_total) if (w_total+l_total) else 0))
     
     return win_rates
 
@@ -42,19 +57,33 @@ def get_battle_stats(df,stats):
     '''
     TODO: Win Streak
     '''
+        
+    gamemodes = set(df.event_mode)
+    gamemodes.difference_update({'unknown',np.nan})
+    
+    print(gamemodes)
+    
+    
+    showdown_places = {'soloShowdown':4,'duoShowdown':2,'trioShowdown':2}
+    
+    
     
     # Avg Trophy gain/loss
-    stats["avg_trophy_change"] = round(df.battle_trophy_change.mean(),2)
+    # stats["avg_trophy_change"] = round(df.battle_trophy_change.mean(),2)
     
     # Most Used Brawler
-    stats["main_brawler"] = df['player_brawler_name'].value_counts().idxmax()
+    # stats["main_brawler"] = df['player_brawler_name'].value_counts().idxmax()
     
     # Win Rates
-    stats['win_rates'] = get_win_rates(df)
+    stats['win_rates'] = get_win_rates(df,gamemodes,showdown_places)
     
     # Calculate Win Streaks
-    # gay = df[df['event_mode'] == ('soloShowdown')]
-    # print(gay)
+    # get_streaks(df,gamemodes,showdown_places)
+    
+    for k,v in stats['win_rates'].items():
+        print(k,v)
+    
+
     
     return stats
 
