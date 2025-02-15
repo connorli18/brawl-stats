0861a9bc94761ad443a150581fa759bcbce5a69a
connorli18 <115739603+connorli18@users.noreply.github.com>
2025-02-15 09:18:37 -0500

    money

--- api_call_brawl_stars.py
+++ api_call_brawl_stars.py
@@ -98,6 +98,7 @@ def get_public_ip_to_config() -> str:
         "128.59.177.135": "brodway_street",
         "128.59.176.37": "board_way2",
         "128.59.178.38": "bway3",
+        "128.59.176.253": "bway4"
     }
 
     try:
