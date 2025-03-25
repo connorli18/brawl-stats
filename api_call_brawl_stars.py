aec9e2b6726182df5a74da593457435f7f2d96b9
connorli18 <115739603+connorli18@users.noreply.github.com>
2025-03-25 15:46:45 -0400

    balls

--- api_call_brawl_stars.py
+++ api_call_brawl_stars.py
@@ -100,7 +100,7 @@ def get_public_ip_to_config() -> str:
         "128.59.176.37": "board_way2",
         "128.59.178.38": "bway3",
         "128.59.176.253": "bway4",
-        "128.59.179.121": "new_ip_columb"
+        "128.59.179.121": "new_ip_colum"
     }
 
     try:
